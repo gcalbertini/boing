6027fefc24de3768ca4a2ad5eab9f3169dfa9ca8
Guilherme A. <47290457+gcalbertini@users.noreply.github.com>
2023-12-03 04:56:58 -0500

    Update to EDA

--- src/sparky.py
+++ src/sparky.py
@@ -135,7 +135,8 @@ os.sys.stdout = os.sys.__stdout__
 
 # We can see from the log files generated that VehBodystyle only has SUV and
 # VehType only has Used. Neither have nulls in the column --> drop redundancy
-colRedundancy = ["VehBodystyle", "VehType"]
+# 8 speed auto or an ambiguous auto for transmissions seems standard
+colRedundancy = ["VehBodystyle", "VehType", "VehTransmission"]
 trainRaw = trainRaw.drop(*colRedundancy)
 testRaw = testRaw.drop(*colRedundancy)
 
@@ -167,6 +168,10 @@ testRaw, trainRaw = [
         lower(regexp_replace(col("VehColorInt"), "[^a-zA-Z0-9]", "")),
     )
     .withColumn(
+        "VehColorExt",
+        lower(regexp_replace(col("VehColorExt"), "[^a-zA-Z0-9]", "")),
+    )
+    .withColumn(
         "VehDriveTrain",
         lower(
             regexp_replace(col("VehDriveTrain"), "[^a-zA-Z0-9/]", "")
@@ -181,9 +186,8 @@ feats.show()
 # Vectorize array 'string' types that may contain richer info and cast for new schema
 testRaw, trainRaw = [
     df.withColumn(
-        "VehColorExt", split(col("VehColorExt"), " ").cast(ArrayType(StringType()))
+        "VehEngine", split(col("VehEngine"), " ").cast(ArrayType(StringType()))
     )
-    .withColumn("VehEngine", split(col("VehEngine"), " ").cast(ArrayType(StringType())))
     .withColumn(
         "VehHistory", split(col("VehHistory"), ",").cast(ArrayType(StringType()))
     )
@@ -219,17 +223,17 @@ print(f"New column for owners contains {nullEntryCount}/201 null entries in trai
 # There are only so many VehDriveTrain options. Replace drive with 'D', all with 'A' etc
 
 testRaw, trainRaw = [
-    df.withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "front", "F"))
-    .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "wheel", "W"))
-    .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "drive", "D"))
-    .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "all", "A"))
+    df.withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "front", "f"))
+    .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "wheel", "w"))
+    .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "drive", "d"))
+    .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "all", "a"))
     .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "four", "4"))
     .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "two", "2"))
     .withColumn("VehDriveTrain", regexp_replace(col("VehDriveTrain"), "or", "/"))
     for df in [testRaw, trainRaw]
 ]
 
-# We note from logs that 'ALL-WHEEL DRIVE WITH LOCKING AND LIMITED-SLIP DIFFERENTIAL' would read as AWDwithlockingandlimitedslipdifferential as such:
+# We note from logs that 'ALL-WHEEL DRIVE WITH LOCKING AND LIMITED-SLIP DIFFERENTIAL' would read as awdwithlockingandlimitedslipdifferential as such:
 funky = (
     trainRaw.select("VehDriveTrain", "ListingID")
     .filter(length(col("VehDriveTrain")) > 7)
@@ -239,8 +243,29 @@ funky.show()
 
 trainRaw = trainRaw.withColumn(
     "VehDriveTrain",
-    when(col("ListingID") == 425217, "AWD").otherwise(col("VehDriveTrain")),
+    when(col("ListingID") == 425217, "awd").otherwise(col("VehDriveTrain")),
 )
+
+funky = (
+    trainRaw.select("VehDriveTrain", "ListingID")
+    .filter(length(col("VehDriveTrain")) < 3)
+    .limit(3)
+)
+funky.show()
+
+trainRaw = trainRaw.withColumn(
+    "VehDriveTrain",
+    when(col("ListingID") == 4199245, "awd").otherwise(col("VehDriveTrain")),
+)
+
+# Lastly also note that 4wd and 4x4 are the same thing; opt to 4wd
+testRaw, trainRaw = [
+    df.withColumn(
+        "VehDriveTrain", regexp_replace("VehDriveTrain", "4x4", "4wd")
+    ).withColumn("VehDriveTrain", regexp_replace("VehDriveTrain", "4wd/4wd", "4wd"))
+    for df in [testRaw, trainRaw]
+]
+
 trainRaw.show(2)
 
 
@@ -254,6 +279,7 @@ print(f"The Test DataFrame is  {testRaw.count()} by {len(testRaw.columns)}.")
 # https://spark.apache.org/docs/latest/ml-features.html
 # https://databricks-prod-cloudfront.cloud.databricks.com/public/4027ec902e239c93eaaa8714f173bcfc/2281281647728580/33322862051354/347222873603598/latest.html
 # https://spark.apache.org/docs/latest/ml-linalg-guide.html << should be installed for faster numerical processing
+# https://neptune.ai/blog/word-embeddings-guide#word2vec
 
 # First nulls should be filled with `None.` as it's likley there is intentionality to notes being left blank and feature has significant heterogeneity even when tokenized
 trainRaw = trainRaw.na.fill("None.", subset=["VehSellerNotes"])
@@ -261,17 +287,19 @@ testRaw = testRaw.na.fill("None.", subset=["VehSellerNotes"])
 
 
 def process_text_column(input_col, trainX, testX, vector_size=20, min_count=3):
-
     # Tokenize the input column of both sets of data
     regexTokenizer = RegexTokenizer(
-        gaps=False, pattern="\w+", inputCol=input_col, outputCol=f"Token1_{input_col}"
+        gaps=False,
+        pattern="\w+",
+        inputCol=input_col,
+        outputCol=f"Word2Vec1_{input_col}",
     )
     trainX = regexTokenizer.transform(trainX)
     testX = regexTokenizer.transform(testX)
 
     # Remove stop words from both sets of data
     swr = StopWordsRemover(
-        inputCol=f"Token1_{input_col}", outputCol=f"Token2_{input_col}"
+        inputCol=f"Word2Vec1_{input_col}", outputCol=f"Word2Vec2_{input_col}"
     )
     trainX = swr.transform(trainX)
     testX = swr.transform(testX)
@@ -280,15 +308,15 @@ def process_text_column(input_col, trainX, testX, vector_size=20, min_count=3):
     word2vec = Word2Vec(
         vectorSize=vector_size,
         minCount=min_count,
-        inputCol=f"Token2_{input_col}",
-        outputCol=f"Token_{input_col}",
+        inputCol=f"Word2Vec2_{input_col}",
+        outputCol=f"Word2Vec_{input_col}",
     )
     model = word2vec.fit(trainX)
     trainX = model.transform(trainX)
     testX = model.transform(testX)
 
     # Drop intermmediaries and original raw form
-    col2Drop = [input_col, f"Token1_{input_col}", f"Token2_{input_col}"]
+    col2Drop = [input_col, f"Word2Vec1_{input_col}", f"Word2Vec2_{input_col}"]
     for c in col2Drop:
         trainX = trainX.drop(c)
         testX = testX.drop(c)
@@ -296,24 +324,35 @@ def process_text_column(input_col, trainX, testX, vector_size=20, min_count=3):
     return trainX, testX
 
 
-# Assume that words must show up at least 10 times in corpus (notes column) to be considered for training and reduce some noise;
+# Assume that note entries (~4000 uniques/6k) have words must show up at least 10 times in corpus (notes column) to be considered for training and reduce some noise;
 # avoid overfitting on this small dataset and keep vec size near 100 --> use another project to fine tune all of this
+# Larger vector sizes might be more suitable when dealing with a ``vast`` vocabulary here or when words have multiple meanings,
+# as the model has more dimensions to differentiate between these nuances. Keep in mind this is a miniature text corpus and that about a third seemed
+# to be duplicate (perhaps standard or saved) note entries from certain dealers
 trainRaw, testRaw = process_text_column(
     "VehSellerNotes", trainRaw, testRaw, vector_size=100, min_count=10
 )
 
+# No nulls in the data for seller cities (1300+ uniques). There is somewhat considerable verbal diversity here and it may be beneficial
+# to capture similarity between cities and understand the relationships between them when predicting the trim-price label
+# (e.g., a 2020 Honda Vroomvroom is $X in these coastal cities and $Y in this other region of cities and has Z% higher price for 2021 model).
+# Let's make sure cities where most cars are sold have greater semantic weight so min count set to 3 in corpus
+# A smaller vector size, such as 30, would result in more compact representations but might capture simpler relationships between names
+trainRaw, testRaw = process_text_column(
+    "SellerCity", trainRaw, testRaw, vector_size=25, min_count=3
+)
 # Since seller name does not have any nulls and 700+ types lets do a last min tokenization (see logs)
 # making sure all names are considered for training. Alt could use something like a dummy variable but introduces
-# a lot of sparsity in the design matrix
+# a lot of sparsity in the design matrix. See similar notes above.
 trainRaw, testRaw = process_text_column(
-    "SellerName", trainRaw, testRaw, vector_size=10, min_count=1
+    "SellerName", trainRaw, testRaw, vector_size=25, min_count=3
 )
+
 
 trainRaw.show(3)
 
-# ListingID is not quite an index and can spill into nonsensical feature contributions; can be indexed in jupyter nb
+# ListingID is not quite an index and can spill into nonsensical feature contributions; can be indexed in jupyter nb; keep for test as will be a needed col
 trainRaw = trainRaw.drop("ListingID")
-testRaw = testRaw.drop("ListingID")
 
 # On the question of missing data. We have to study MAR, MCAR, or MNAR.
 # https://bookdown.org/rwnahhas/RMPH/mi-mechanisms.html
