d27f1271a15df9eb86bd1a7f77996b2a168edc52
Guilherme A. <47290457+gcalbertini@users.noreply.github.com>
2023-12-06 02:16:51 -0500

    fixes dummy variable treap

--- src/multiLR.py
+++ src/multiLR.py
@@ -1,4 +1,47 @@
-import torch
-import torch.nn as tnn
+## Standard libraries
+import os
+import json
+import math
 import numpy as np
+
+## Imports for plotting
 import matplotlib.pyplot as plt
+%matplotlib inline
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('svg', 'pdf') # For export
+import seaborn as sns
+sns.set()
+
+## Progress bar
+from tqdm.notebook import tqdm
+
+## PyTorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+
+# Path to the folder where the datasets are/should be downloaded (e.g. MNIST)
+DATASET_PATH = "../data"
+# Path to the folder where the pretrained models are saved
+CHECKPOINT_PATH = "../saved_models/"
+
+# Function for setting the seed
+def set_seed(seed):
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    if torch.cuda.is_available(): # GPU operation have separate seed
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+set_seed(42)
+
+# Additionally, some operations on a GPU are implemented stochastic for efficiency
+# We want to ensure that all operations are deterministic on GPU (if used) for reproducibility
+torch.backends.cudnn.deterministic = True #  bool that, if True, causes cuDNN to only use deterministic convolution algorithms.
+torch.backends.cudnn.benchmark = False # A bool that, if True, causes cuDNN to benchmark multiple convolution algorithms and select the fastest.
+
+# Fetching the device that will be used throughout
+device = torch.device("cpu") if not torch.cuda.is_available() else torch.device("cuda:0")
+print("Using device", device)
+
