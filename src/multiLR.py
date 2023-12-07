08faf106abdc9a835f45573daa523d83adabe6c5
Guilherme A. <47290457+gcalbertini@users.noreply.github.com>
2023-12-07 07:21:39 -0500

    final stretch

--- src/multiLR.py
+++ src/multiLR.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import seaborn as sns
 from torch.nn.functional import softmax
+from datetime import datetime
 
 sns.set()
 
@@ -21,7 +22,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils.data as data
 
-DATASET_PATH = "./data"
+DATASET_PATH = "./data/"
 # Path to the folder where the pretrained models are saved
 CHECKPOINT_PATH = "./saved_models/"
 
@@ -69,19 +70,25 @@ class car_data(data.Dataset):
 
     def generate_data(self):
         X_train = np.loadtxt(
-            "./data/py_training_X.csv", delimiter=",", dtype=np.float32
+            DATASET_PATH + "/py_training_X.csv", delimiter=",", dtype=np.float32
         )  # float54 used in nb but 32 is what pytorch model takes
-        X_test = np.loadtxt("./data/py_training_X.csv", delimiter=",", dtype=np.float32)
-        X_features = np.loadtxt(
-            "./data/py_X_ft_names.csv", delimiter=",", dtype=np.str_
+        X_test = np.loadtxt(
+            DATASET_PATH + "/py_training_X.csv", delimiter=",", dtype=np.float32
         )
-        y_raw = np.loadtxt("./data/py_y_train.csv", delimiter=",", dtype=np.str_)
-        y_enc = np.loadtxt("./data/py_y_enc_train.csv", delimiter=",", dtype=np.float32)
+        X_features = np.loadtxt(
+            DATASET_PATH + "/py_X_ft_names.csv", delimiter=",", dtype=np.str_
+        )
+        y_raw = np.loadtxt(
+            DATASET_PATH + "/py_y_train.csv", delimiter=",", dtype=np.str_
+        )
+        y_enc = np.loadtxt(
+            DATASET_PATH + "/py_y_enc_train.csv", delimiter=",", dtype=np.float32
+        )
         y_enc_features = np.loadtxt(
-            "./data/py_y_enc_ft_names.csv", delimiter=",", dtype=np.str_
+            DATASET_PATH + "/py_y_enc_ft_names.csv", delimiter=",", dtype=np.str_
         )
         self.listingID = np.loadtxt(
-            "./data/py_test_id.csv", delimiter=",", dtype="i8"
+            DATASET_PATH + "/py_test_id.csv", delimiter=",", dtype="i8"
         )  # 64-bit signed integer used in nb
 
         self.X_test = torch.from_numpy(X_test)
@@ -131,18 +138,29 @@ n_samples = len(dataset)
 print("Size of dataset:", n_samples)
 print("Data point 0:", dataset[0])
 
+
 train_size = int(0.8 * n_samples)
-test_size = n_samples - train_size
-batch_size = 64
-learning_rate = 0.00008
+val_size = n_samples - train_size
+train_dataset, val_dataset = data.random_split(dataset, [train_size, val_size])
+
+batch_size = 2**7
+learning_rate = 0.0001
 weight_decay = 0
 momentum = 0
 
 
-n_iterations = math.ceil(n_samples / batch_size)
 train_data_loader = data.DataLoader(
-    dataset=dataset, batch_size=batch_size, shuffle=True
+    dataset=train_dataset,
+    batch_size=batch_size,
+    shuffle=True,  # Shuffle the training set during training
 )
+
+val_data_loader = data.DataLoader(
+    dataset=val_dataset,
+    batch_size=batch_size,
+    shuffle=False,  # Do not shuffle the validation set during validation
+)
+
 
 # next(iter(...)) catches the first batch of the data loader
 # If shuffle is True, this will return a different batch every time we run this cell
@@ -168,18 +186,24 @@ class MLT_log_reg(nn.Module):
         super(MLT_log_reg, self).__init__()
         self.trim_linear = nn.Linear(n_input_features, num_trim_classes)
         self.price_linear = nn.Linear(n_input_features, 1)
-        #Fight off co-adaptation (when multiple neurons in a layer extract the same, or very similar, hidden features from the input data)
+        # Fight off co-adaptation (when multiple neurons in a layer extract the same, or very similar, hidden features from the input data)
         # So p% of nodes in input and hidden layer dropped in every iteration (batch) --> sparsity
         # By using dropout, in every iteration, you will work on a smaller neural network
-        # than the previous one and, therefore, approaches regularization which (when using MSE)
+        # than the previous one and, therefore, approaches regularization which (if using MSE)
         # shrinks the squared norm of the weights --> reduce overfitting
+
+        # Define learnable parameters for trim_weight and price_weight
+        # https://arxiv.org/abs/2004.13379
+        self.trim_weight = nn.Parameter(torch.Tensor([1.0]))
+        self.price_weight = nn.Parameter(torch.Tensor([1.0]))
         self.dropout = nn.Dropout(p=0.1)
 
     def forward(self, x):
         x = self.dropout(x)
         trim_non_logits = self.trim_linear(x)
         price_vals = self.price_linear(x)
-        return softmax(trim_non_logits), price_vals
+        # cross entropy loss will normalize the logits across class
+        return torch.sigmoid(trim_non_logits), price_vals
 
 
 model = MLT_log_reg(n_input_features, num_trim_classes=num_trim_classes)
@@ -187,19 +211,38 @@ model.to(device)
 
 # NOTE reduction by mean default means loss will be normalized by batch size
 # to get a loss per epoch can set to sum or mult by batch size and then divide by
-# entire dataset size 
+# entire dataset size
 # CrossEntropyLoss combines nn.LogSoftmax() in model followed by nn.NLLLoss() criterion in one single go assuming input is logits (softmax'd)
-# The softmax function returns probabilities between [0, 1] where sum *across classes* sum to 1; log of these probabilities 
+# The softmax function returns probabilities between [0, 1] where sum *across classes* sum to 1; log of these probabilities
 # returns values between [-inf, 0], since log(0) = -inf and log(1) = 0
-loss_trim = nn.CrossEntropyLoss() # need input in logits
-loss_price = nn.MSELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay)
+trim_criterion = nn.CrossEntropyLoss()  # need input in logits
+price_criterion = (
+    nn.HuberLoss()
+)  # see notes in EDA; we did not sift out outliers in totality for hetereroscedastic priors
+optimizer = torch.optim.SGD(
+    model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=weight_decay
+)
 print("The parameters: ", list(model.parameters()))
 
-nn.NLLLoss
-def train_model(model, optimizer, train_data_loader, loss_price, loss_trim, trim_weight = 1.0, price_weight = 1.0, num_epochs=50):
+
+def train_model(
+    model,
+    optimizer,
+    train_data_loader,
+    price_loss,
+    trim_loss,
+    num_epochs=50,
+    val_data_loader=val_data_loader,
+    patience=25,
+):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     # Set model to train mode
     model.train()
+
+    # Initialize early stopping parameters
+    best_loss = float("inf")
+    consecutive_no_improvement = 0
+
     # Training loop
     for epoch in range(num_epochs):
         with tqdm(
@@ -217,12 +260,22 @@ def train_model(model, optimizer, train_data_loader, loss_price, loss_trim, trim
                 ## sigmoid for us
                 pred_trim_logits, pred_price_vals = model(features)
 
-                ## Step 3: Calculate the loss --> recall -log(p) for BCE means anything near .693
-                # is about random aka about 50% samples correctly classified 
-                trim_loss = loss_trim(pred_trim_logits, trim_labels) # loss we chose here will apply log to logits then get negative log likelihood loss
-                price_loss = loss_price(pred_price_vals, price_labels)
+                # Access learnable parameters
+                trim_weight = model.trim_weight
+                price_weight = model.price_weight
 
-                total_loss =  price_weight * price_loss #trim_weight * trim_loss +
+                ## Step 3: Calculate the loss --> recall -log(p) for BCE means anything near .693
+                # is about random aka about 50% samples correctly classified
+                trim_loss = trim_criterion(
+                    pred_trim_logits, trim_labels
+                )  # loss we chose here will apply softmax to logits then get negative log likelihood loss
+                price_loss = price_criterion(pred_price_vals, price_labels)
+
+                total_loss = (
+                    price_loss / (2 * price_weight**2)
+                    + trim_loss / (2 * trim_weight**2)
+                    + torch.log(trim_weight * price_weight)
+                )
 
                 ## Step 4: Perform backpropagation
                 # Before calculating the gradients, we need to ensure that they are all zero.
@@ -233,50 +286,90 @@ def train_model(model, optimizer, train_data_loader, loss_price, loss_trim, trim
                 ## Step 5: Update the parameters
                 optimizer.step()
 
-                pbar.update(1)  
-                pbar.set_postfix({"Total Loss": total_loss.item(), "Trim Loss": trim_loss.item(), "Price Loss": price_loss.item()})
+                pbar.update(1)
+                pbar.set_postfix(
+                    {
+                        "Total Loss": total_loss.item(),
+                        "Trim Loss (CrossEnt)": trim_loss.item(),
+                        "Price Loss (Huber)": price_loss.item(),
+                    }
+                )
 
-train_model(model, optimizer, train_data_loader, loss_price, loss_trim)
+        # Evaluate on the validation set if provided
+        if val_data_loader is not None:
+            val_metrics = evaluate_model(model, val_data_loader, price_loss, trim_loss)
+            val_loss = val_metrics[
+                "Total Loss"
+            ]  # Adjust this based on metric for early stopping
+            print(f"Validation Loss after epoch {epoch + 1}: {val_loss}\n")
 
-"""
+            # Check for early stopping
+            if val_loss < best_loss:
+                best_loss = val_loss
+                consecutive_no_improvement = 0
+            else:
+                consecutive_no_improvement += 1
+
+            if consecutive_no_improvement >= patience or epoch == num_epochs - 1:
+                print(f"Saving model weights after epoch {epoch + 1}.")
+                # Save the model weights
+                model_path = "model_{}_{}".format(timestamp, epoch)
+                if not os.path.exists(CHECKPOINT_PATH):
+                    os.makedirs(CHECKPOINT_PATH)
+                torch.save(model.state_dict(), CHECKPOINT_PATH + model_path)
+                break
 
 
+def evaluate_model(model, data_loader, price_loss, trim_loss):
+    # Set model to evaluation mode
+    model.eval()
 
-# Optionally, save the trained model
-torch.save(model.state_dict(), "trained_model.pth")
+    total_price_loss = 0.0
+    total_trim_loss = 0.0
 
-state_dict = model.state_dict()
-print(state_dict)
+    with torch.no_grad():
+        for batch_idx, batch in enumerate(data_loader):
+            features = batch["features"].to(device)
+            trim_labels = batch["labels"]["trim_label"].to(device)
+            price_labels = batch["labels"]["price_label"].to(device)
 
-test_dataset = dataset.get_test_set()
-# drop_last -> Don't drop the last batch although it is smaller than 128
-test_data_loader = data.DataLoader(
-    test_dataset, batch_size=128, shuffle=False, drop_last=False
+            # Forward pass
+            pred_trim_logits, pred_price_vals = model(features)
+
+            # Calculate losses
+            trim_loss = trim_criterion(pred_trim_logits, trim_labels)
+            price_loss = price_criterion(pred_price_vals, price_labels)
+
+            # https://stackoverflow.com/questions/54053868/how-do-i-get-a-loss-per-epoch-and-not-per-batch
+            total_trim_loss += trim_loss.item()
+            total_price_loss += price_loss.item()
+
+    # Set model back to train mode
+    model.train()
+
+    # Return average losses over the validation set
+    avg_trim_loss = total_trim_loss / len(data_loader)
+    avg_price_loss = total_price_loss / len(data_loader)
+
+    return {
+        "Trim Loss (CrossEnt)": avg_trim_loss,
+        "Price Loss (Huber)": avg_price_loss,
+        "Total Loss": avg_trim_loss + avg_price_loss,
+    }
+
+
+# Example usage:
+train_model(
+    model,
+    optimizer,
+    train_data_loader,
+    price_criterion,
+    trim_criterion,
+    num_epochs=10,
 )
 
-
-def eval_model(model, data_loader):
-    model.eval()  # Set model to eval mode
-    true_preds, num_preds = 0.0, 0.0
-
-    with torch.no_grad():  # Deactivate gradients for the following code
-        for data_inputs, data_labels in data_loader:
-            # Determine prediction of model on dev set
-            data_inputs, data_labels = data_inputs.to(device), data_labels.to(device)
-            preds = model(data_inputs)
-            preds = preds.squeeze(dim=1)
-            preds = torch.sigmoid(preds)  # Sigmoid to map predictions between 0 and 1
-            pred_labels = (preds >= 0.5).long()  # Binarize predictions to 0 and 1
-
-            # Keep records of predictions for the accuracy metric (true_preds=TP+TN, num_preds=TP+TN+FP+FN)
-            true_preds += (pred_labels == data_labels).sum()
-            num_preds += data_labels.shape[0]
-
-    acc = true_preds / num_preds
-    print(f"Accuracy of the model: {100.0*acc:4.2f}%")
-
-    eval_model(model, test_data_loader)
-"""
+# val_metrics = evaluate_model(model, val_data_loader, price_loss, trim_loss)
+# print(f"Validation Metrics: {val_metrics}")
 
 
 if __name__ == "__main__":
